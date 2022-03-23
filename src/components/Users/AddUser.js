9cdaa7d1ebc0583c1d8cf2d72339a3089dd0c324
GlennOnyango <glennonyango@yahoo.com>
2022-03-23 15:10:06 +0300

    capturing form data

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,19 +1,31 @@
+import {useState} from 'react';
 import Container from "../UI/Container";
 import Button from "../UI/Button";
 import styles from "./AddUser.module.css";
 const AddUser = ()=>{
 
+
+    const [enteredUserName,setUserName] = useState('');
+    const [enteredAge,setAge] = useState();
+
     const addUserHandler = (e)=>{
         e.preventDefault();
+        setUserName('');
+        setAge();
     }
-
+    const addUserName = (e)=>{
+        setUserName(e.target.value);
+    }
+    const addAge = (e)=>{
+        setAge(e.target.value);
+    }
     return(
         <Container className={styles.input}>
         <form onSubmit={addUserHandler}>
             <label htmlFor="username"></label>
-            <input type="text" id="username"/>
+            <input type="text" id="username" onChange={addUserName}/>
             <label htmlFor="age">Age(Years)</label>
-            <input type="text" id="age"/>
+            <input type="text" id="age" onChange={addAge}/>
             <Button type="submit">Add User</Button>
         </form>
         </Container>
